f2a27f99028a5bbe3e5d6023c2b98eb6330d722f
Shakthi Weerasinghe <yasas276@gmail.com>
2025-02-26 16:24:28 +1100

    Initial commit

---
+++ api.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+import os
+
+app = Flask(__name__)
+
+# Azure Cosmos DB for MongoDB connection string
+COSMOS_DB_URI = 'https://dcisdatabase.mongo.cosmos.azure.com:443/'
+DATABASE_NAME = 'dcis'
+
+# MongoDB client initialization
+client = MongoClient(COSMOS_DB_URI)
+db = client[DATABASE_NAME]
+
+@app.route('/enclosures', methods=['GET'])
+def getEnclosureMetaData():
+    try:
+        # Retrieving the data from the situations collection.
+        collection = db['situations']
+        args = request.args
+        if(len(args)>0):
+            # Retrieving the enclosure tag.
+            tag = args['tag']
+            query = {'tag':tag}
+            data = collection.read_single(query)
+
+            return jsonify(data), 200
+        else:
+            # Retrieving all the enclosures
+            data = list(collection.find({}, {'_id':0}))
+
+            return jsonify(data), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
